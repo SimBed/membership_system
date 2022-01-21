487542e78f6ff7524618b4d3dadd518440a07c6a
SimBed <dansimbed@gmail.com>
2022-01-21 23:20:15 +0530

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/purchases/clear_filters', to: 'admin/purchases#clear_filters', as: 'clear_filters'
   # note (check this is true) if the 'get' is not before the 'resources', the get purchases/search will be handled by the show method (with params[:id] = 'search')
   get '/purchases/filter', to: 'admin/purchases#filter', as: 'purchase_filter'
-  get '/wkclasses/filter', to: 'wkclasses#filter'
+  get '/wkclasses/filter', to: 'admin/wkclasses#filter'
   post '/products/payment', to: 'admin/products#payment'
   get    '/login',   to: 'auth/sessions#new'
   post   '/login',   to: 'auth/sessions#create'
