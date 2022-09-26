c3e9364482a4adcacd4a5df5bbe69a7d1740cea4
SimBed <dansimbed@gmail.com>
2022-09-26 20:40:56 +0100

    remove git merge headers from schema after merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2022_09_07_185954) do
-=======
 ActiveRecord::Schema.define(version: 2022_09_14_114358) do
->>>>>>> clientrazor
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
