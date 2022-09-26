33be35f37b95e274235e1e98bb037a4a87b286f1
SimBed <dansimbed@gmail.com>
2022-09-26 20:51:59 +0100

    remove razor seed file

--- app/models/concerns/order_concerns/seeds.rb
+++
@@ -1,3 +0,0 @@
-Seller.create(name:"Ravi Kumar",description: Faker::Lorem::sentence,email: "ravi@gmail.com",user_id: User.first.id)
-Product.create(title: "Rubic Cube",price: 100,description: Faker::Lorem::paragraph(sentence_count: 5),seller_id: Seller.first.id)
-Product.create(title: "Head Phones",price: 100,description: Faker::Lorem::paragraph(sentence_count: 5),seller_id: Seller.first.id)
