44c3e9147702049f634e100a019b7942a76b7211
SimBed <dansimbed@gmail.com>
2023-04-27 13:13:54 +0530

    add superadmin tests to password reset tests

--- test/integration/password_reset_test.rb
+++ test/integration/password_reset_test.rb
@@ -5,19 +5,18 @@ class PasswordResetTest < ActionDispatch::IntegrationTest
     @account = accounts(:client_for_unlimited)
     @client = @account.client
     @admin = accounts(:admin)
+    @superadmin = accounts(:superadmin)
   end
 
   test 'admin resets password' do
-    @account.update(password: 'password', password_confirmation: 'password')
     assert @account.authenticate('password')
     log_in_as(@admin)
+    # admin cannot specify the actual password. The new password is randoml;y generated on update.
     patch admin_account_path(@account)
     refute @account.reload.authenticate('password')
   end
 
   test 'client resets password with valid password' do
-    @account.update(password: 'password', password_confirmation: 'password')
-    assert @account.authenticate('password')
     log_in_as(@account)
     patch admin_account_path(@account), params: { account: { new_password: 'foobar', new_password_confirmation: 'foobar' } }
     refute @account.reload.authenticate('password')
@@ -25,8 +24,6 @@ class PasswordResetTest < ActionDispatch::IntegrationTest
   end
 
   test 'client resets password with invalid password' do
-    @account.update(password: 'password', password_confirmation: 'password')
-    assert @account.authenticate('password')
     log_in_as(@account)
     patch admin_account_path(@account), params: { account: { new_password: 'fooba', new_password_confirmation: 'fooba' } }
     assert @account.reload.authenticate('password')
@@ -35,12 +32,26 @@ class PasswordResetTest < ActionDispatch::IntegrationTest
   end
 
   test 'client resets password with invalid password/password confirmation combo' do
-    @account.update(password: 'password', password_confirmation: 'password')
-    assert @account.authenticate('password')
     log_in_as(@account)
     patch admin_account_path(@account), params: { account: { new_password: 'foobar', new_password_confirmation: 'barfoo' } }
     assert @account.reload.authenticate('password')
     refute @account.reload.authenticate('foobar')
     assert_template 'client/clients/show'
   end
+
+  test 'superadmin resets admin account password with valid password' do
+    log_in_as(@superadmin)
+    patch admin_account_path(@admin), params: { account: { new_password: 'foobar', new_password_confirmation: 'foobar', requested_by: 'superadmin_of_admin' } }
+    assert @admin.reload.authenticate('foobar')
+    refute @admin.reload.authenticate('password')
+  end
+
+  test 'superadmin resets admin account password with invalid password' do
+    log_in_as(@superadmin)
+    patch admin_account_path(@admin), params: { account: { new_password: 'fooba', new_password_confirmation: 'fooba', requested_by: 'superadmin_of_admin' } }
+    assert @admin.reload.authenticate('password')
+    refute @admin.reload.authenticate('fooba')
+    assert_redirected_to admin_accounts_path    
+  end
+
 end
