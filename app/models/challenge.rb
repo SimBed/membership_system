8c0f8804d5e4e7200a9a47aa25ab399ee73c3060
SimBed <dansimbed@gmail.com>
2023-06-30 12:51:32 +0530

    reformat challenge results data

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -11,28 +11,36 @@ class Challenge < ApplicationRecord
 
   def results
     if has_sub_challenges?
-      sub_challenges.map { |c| c.results } # 1
-                    .map {|r| r.to_a} # 2
-                    .flatten.group_by {|r| r['id']} # 3
-                    .map { |key, value| value.reduce { |acc, h| (acc || {}).merge(h) { |key, oldval, newval| key=='max_score' ? (oldval + newval) : oldval } }} # 4
-                    .sort_by {|h| -h['max_score']}
-    # 1 array of ActiveRecord::Results
-    # 2 array of array of hashes [ [...], [{"id"=>41, "first_name"=>"Anne", "max_score"=>3850}, {"id"=>209, "first_name"=>"Aakash", "max_score"=>2500}],...]
-    # 3 hash of clients results {41=>[{"id"=>41, "first_name"=>"Anne", "max_score"=>3901}, {"id"=>41, "first_name"=>"Anne", "max_score"=>3850}], 209=>[...],...}    
-    # 4 reduce each array of hashes to a single hash, showing the aggregated client score
+      main_challenge_results
     else
       sub_challenge_results
     end
   end
+  
+  def main_challenge_results
+    sub_challenge_ids = sub_challenges.pluck(:id).join(',')
+    sql = "SELECT unaggregated.id, unaggregated.first_name, sum(max_score) AS sum_max_score
+           FROM (
+            SELECT clients.id, clients.first_name, max(achievements.score) AS max_score
+            FROM clients
+            JOIN achievements ON clients.id = achievements.client_id
+            WHERE challenge_id IN (#{sub_challenge_ids})
+            GROUP BY achievements.challenge_id, clients.id
+            ) AS unaggregated
+          GROUP BY unaggregated.id, unaggregated.first_name
+          ORDER BY sum_max_score DESC;"
+
+      ActiveRecord::Base.connection.exec_query(sql)
+  end
 
   def sub_challenge_results
     sql = "SELECT clients.id, clients.first_name, max(achievements.score) as max_score
-          FROM clients
-          INNER JOIN achievements on clients.id = achievements.client_id
-          INNER JOIN challenges on achievements.challenge_id = challenges.id
-          WHERE challenges.id = #{id}
-          GROUP BY clients.id
-          ORDER BY max_score DESC;"
+           FROM clients
+           INNER JOIN achievements on clients.id = achievements.client_id
+           INNER JOIN challenges on achievements.challenge_id = challenges.id
+           WHERE challenges.id = #{id}
+           GROUP BY clients.id
+           ORDER BY max_score DESC;"
 
       ActiveRecord::Base.connection.exec_query(sql)
   end
@@ -43,14 +51,19 @@ class Challenge < ApplicationRecord
     false
   end
 
-  # def positions_for_main
-  #     sql = "SELECT clients.id, clients.first_name, max(achievements.score) as max_score
-  #           FROM clients
-  #           INNER JOIN achievements on clients.id = achievements.client_id
-  #           INNER JOIN challenges on achievements.challenge_id = challenges.id
-  #           WHERE challenges.id = #{id}
-  #           GROUP BY clients.id
-  #           ORDER BY max_score DESC;"
-  #     ActiveRecord::Base.connection.exec_query(sql)
-  # end
+  # def results
+  #   if has_sub_challenges?
+  #     sub_challenges.map { |c| c.results } # 1
+  #                   .map {|r| r.to_a} # 2
+  #                   .flatten.group_by {|r| r['id']} # 3
+  #                   .map { |key, value| value.reduce { |acc, h| (acc || {}).merge(h) { |key, oldval, newval| key=='max_score' ? (oldval + newval) : oldval } }} # 4
+  #                   .sort_by {|h| -h['max_score']}
+  #   # 1 array of ActiveRecord::Results
+  #   # 2 array of array of hashes [ [...], [{"id"=>41, "first_name"=>"Anne", "max_score"=>3850}, {"id"=>209, "first_name"=>"Aakash", "max_score"=>2500}],...]
+  #   # 3 hash of clients results {41=>[{"id"=>41, "first_name"=>"Anne", "max_score"=>3901}, {"id"=>41, "first_name"=>"Anne", "max_score"=>3850}], 209=>[...],...}    
+  #   # 4 reduce each array of hashes to a single hash, showing the aggregated client score
+  #   else
+  #     sub_challenge_results
+  #   end
+  # end  
 end
--- lib/tasks/wkclass_maker.rake
+++ lib/tasks/wkclass_maker.rake
@@ -2,7 +2,7 @@ desc 'create wkclasses for the day based on timetable'
 task wkclass_daily_create: :environment do
   wkclass_date = Time.zone.now.advance(days: Setting.classmaker_advance)
   day_of_week = wkclass_date.strftime('%A')
-  entries = Entry.joins(table_day: [:timetable]).joins(:table_time).where(timetables: { id: Setting.timetable }).where(table_days: { name: day_of_week }).order('table_times.start')
+  entries = Entry.joins(table_day: [:timetable]).joins(:table_time).where(timetables: { id: Rails.application.config_for(:constants)['timetable_id'] }).where(table_days: { name: day_of_week }).order('table_times.start')
   entries.each do |entry|
     start = entry.table_time.start
     max_capacity = entry.studio == 'Window' ? 8 : 12
