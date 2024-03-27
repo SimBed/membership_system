31623b91688ba118653a1172a029b3177467d993
SimBed <dansimbed@gmail.com>
2024-03-27 08:55:31 +0530

    start timetable  at Monday

--- app/models/table_day.rb
+++ app/models/table_day.rb
@@ -2,19 +2,20 @@ class TableDay < ApplicationRecord
   belongs_to :timetable
   has_many :entries, dependent: :destroy
   before_save :upcase_names
+  # Rails 7 introduces ActiveRecord::QueryMethods#in_order_of making previous SQL approach reundant
   # https://www.rubyinrails.com/2021/12/14/fetch-records-in-custom-order-with-rails-activerecord/
   # use ruby to build the sql query order('CASE day WHEN 'Monday' then 0 WHEN 'Tuesday' then 1...END)
-  # NOTE: Rails 7 introduces in_order_of method, which can replace this after upgrade
-  SORT_ORDER = Date::DAYNAMES.rotate(Time.zone.today.cwday)
-  scope :order_by_day, lambda {
-                         order_clause = 'CASE name '
-                         SORT_ORDER.each_with_index do |day, index|
-                           order_clause << sanitize_sql_array(['WHEN ? THEN ? ', day, index])
-                         end
-                         order_clause << sanitize_sql_array(['ELSE ? END', SORT_ORDER.length])
-                         order(Arel.sql(order_clause))
-                       }
-
+  # SORT_ORDER = Date::DAYNAMES.rotate(Time.zone.today.cwday) #timetable's first column is today's day
+  SORT_ORDER = Date::DAYNAMES.rotate(1) #timetable's first column is Monday
+  # scope :order_by_day, lambda {
+  #                        order_clause = 'CASE name '
+  #                        SORT_ORDER.each_with_index do |day, index|
+  #                          order_clause << sanitize_sql_array(['WHEN ? THEN ? ', day, index])
+  #                        end
+  #                        order_clause << sanitize_sql_array(['ELSE ? END', SORT_ORDER.length])
+  #                        order(Arel.sql(order_clause))
+  #                      }
+  scope :order_by_day, -> { in_order_of(:name, SORT_ORDER) }
   private
 
   def upcase_names
