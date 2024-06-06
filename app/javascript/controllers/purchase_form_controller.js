fafea6fc9c953a1cc0cb476de1ab2ff796b99c0a
SimBed <dansimbed@gmail.com>
2024-06-06 20:13:29 +0100

    improve purchase form so changes update date related items, discounts/price

--- app/javascript/controllers/purchase_form_controller.js
+++ app/javascript/controllers/purchase_form_controller.js
@@ -4,13 +4,13 @@ export default class extends Controller {
   static targets = [ "clientSearch", "clientSelect", 'product', "renewalDiscount", "statusDiscount", "commercialDiscount", "discretionDiscount", "oneOffDiscount", "dop", "priceId", "basePrice", "charge"]
   static values = { discountUrl: String, dopChangeUrl: String, clientFilterUrl: String }
 
+  // connect() {
+  //   console.log(this.priceIdTarget)
+  //   console.log(this.basePriceTarget)
+  //   console.log(this.chargeTarget)
+  //   console.log(this.dopTargets[2].value)
+  // }
 
-  connect() {
-    console.log(this.priceIdTarget)
-    console.log(this.basePriceTarget)
-    console.log(this.chargeTarget)
-    console.log(this.dopTargets[2].value)
-  }
   client_search() {
     let client_dropdown = this.clientSelectTarget
 
@@ -22,32 +22,23 @@ export default class extends Controller {
       })
     });    
   }
-
-  // when the dop changes both he the change and the date_change method are called. However, we need the date_change action to complete in its entirety
-  // (which finishes with populating the discount dropdowns) before starting the change method, so set a short timeout on the change method,
-  // before it starts so the date_change method will have time to finish before the change method starts. Improve this as there must be a better way.
-  change() {
-    setTimeout(() => this.load(), 100)
-  }
   
-  date_change() {
-    this.date_change_load();
-  }
-
-  // repopulate the discount dropdowns based on the discounts available at the new date
-  date_change_load(){
+  field_change(){
     let renewal_discount_select = this.renewalDiscountTarget    
     let status_discount_select = this.statusDiscountTarget
     let commercial_discount_select = this.commercialDiscountTarget
     let discretion_discount_select = this.discretionDiscountTarget
     let oneoff_discount_select = this.oneOffDiscountTarget
+    let priceIdEl = this.priceIdTarget;
+    let basePriceEl = this.basePriceTarget;
+    let chargeEl = this.chargeTarget;  
     let queryHash = {
       renewal_discount_id: renewal_discount_select.value,
       status_discount_id: status_discount_select.value,
       commercial_discount_id: commercial_discount_select.value,
       discretion_discount_id: discretion_discount_select.value,
       oneoff_discount_id: oneoff_discount_select.value,
-      // product_id: this.productTarget.value || 0,
+      product_id: this.productTarget.value || 0,
       dop_1i: this.dopTargets[0].value,
       dop_2i: this.dopTargets[1].value,
       dop_3i: this.dopTargets[2].value  
@@ -55,39 +46,24 @@ export default class extends Controller {
     fetch(this.dopChangeUrlValue + '?' + new URLSearchParams(queryHash))
     .then(function(response) {
       response.text().then((s) => {
+        console.log(s);
         renewal_discount_select.innerHTML = JSON.parse(s).renewal;
         status_discount_select.innerHTML = JSON.parse(s).status;
         commercial_discount_select.innerHTML = JSON.parse(s).commercial;
         discretion_discount_select.innerHTML = JSON.parse(s).discretion;
         oneoff_discount_select.innerHTML = JSON.parse(s).oneoff;
-      })
-    });    
-    
-  }
-
-  // repopulate the price related items based on the new product/discount/date (noting the discounts may have already auto-changed if the changed item was dop)
-  load() {
-    let priceIdEl = this.priceIdTarget;
-    let basePriceEl = this.basePriceTarget;
-    let chargeEl = this.chargeTarget;
-    // let basePriceEl = document.getElementById('purchase_base_price');
-    // let paymentEl = document.getElementById('purchase_charge');
-    // let priceIdEl = document.getElementById('purchase_price_id');
-    let queryHash = {
-      renewal_discount_id: this.renewalDiscountTarget.value,
-      status_discount_id: this.statusDiscountTarget.value,
-      commercial_discount_id: this.commercialDiscountTarget.value,
-      discretion_discount_id: this.discretionDiscountTarget.value,
-      oneoff_discount_id: this.oneOffDiscountTarget.value,
-      product_id: this.productTarget.value || 0
-    }
-    // console.log(this.discountUrlValue + '?' + new URLSearchParams(queryHash))
-    fetch(this.discountUrlValue + '?' + new URLSearchParams(queryHash))
-    .then(function(response) {
-      response.text().then((s) => {console.log(s);
         priceIdEl.value = JSON.parse(s).base_price_id;
         basePriceEl.value = JSON.parse(s).base_price_price;
-        chargeEl.value = JSON.parse(s).payment_after_discount;})
-    });
+        chargeEl.value = JSON.parse(s).payment_after_discount;        
+      })
+    });    
   }
+
+  // when the dop changes both the change and the date_change method are called. However, we need the date_change action to complete in its entirety
+  // (which finishes with populating the discount dropdowns) before starting the change method, so set a short timeout on the change method,
+  // before it starts so the date_change method will have time to finish before the change method starts.
+  // change() {
+  //   setTimeout(() => this.load(), 100)
+  // }
+  
 }
