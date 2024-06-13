cc691a71c2ad49b985d8886ac893672ec9f243f7
SimBed <dansimbed@gmail.com>
2024-06-13 10:29:22 +0100

    add workout group filter to workouts index

--- app/controllers/admin/workouts_controller.rb
+++ app/controllers/admin/workouts_controller.rb
@@ -3,7 +3,9 @@ class Admin::WorkoutsController < Admin::BaseController
 
   def index
     @workouts = Workout.order_by_current
-    @workouts = @workouts.current if session[:filter_workout_active].present?
+    handle_filter
+    # @workouts = @workouts.current if session[:filter_workout_active].present?
+    prepare_items_for_filters
     respond_to do |format|
       format.html
       format.turbo_stream
@@ -60,7 +62,36 @@ class Admin::WorkoutsController < Admin::BaseController
     redirect_to workouts_path
   end
 
+  def filter
+    clear_session(*session_filter_list)
+    params_filter_list.each do |item|
+      session["filter_#{item}".to_sym] = params[item]
+    end
+    redirect_to workouts_path
+  end  
+
   private
+
+  def params_filter_list
+    [:in_workout_group, :current]
+  end
+
+  def session_filter_list
+    params_filter_list.map { |i| "filter_#{i}" }
+  end
+
+  def handle_filter
+     %w[current].each do |key|
+      @workouts = @workouts.send(key) if session["filter_#{key}"].present?
+    end
+    %w[in_workout_group].each do |key|
+      @workouts = @workouts.send(key, session["filter_#{key}"]) if session["filter_#{key}"].present?
+    end
+  end  
+
+  def prepare_items_for_filters
+    @workout_group = WorkoutGroup.distinct.pluck(:name).sort!
+  end  
 
   def prepare_items_for_dropdowns
     @capacities = (0..30).to_a + [500]
