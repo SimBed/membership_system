46158679960208cde7468cf1bc9eabbcc68ca70e
SimBed <dansimbed@gmail.com>
2024-08-09 14:46:23 +0100

    hotfix proceed_to_completion method and razor form shop product description

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,11 @@ class Order #< ApplicationRecord
 
   class << self
     def proceed_to_completion(payment_id)
-      orig_payment = Razorpay::Payment.fetch(payment_id)
-      orig_payment.capture(amount: payment.amount) if orig_payment.status == 'authorised'
       payment = Razorpay::Payment.fetch(payment_id)
+      if payment.status == 'authorised'
+        payment.capture(amount: orig_payment.amount) 
+        payment = Razorpay::Payment.fetch(payment_id)
+      end
       return true if payment.status == 'captured' || payment.method =='upi' # upi payments fail to capture (but money still taken from client) causing completion not to occur and subequent admin intervention
     end
 
